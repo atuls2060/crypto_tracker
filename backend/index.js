5fc77b512b61ad321d29ec5d6d96e3b13e287881
Atul Singh <atuls2060@gmail.com>
2023-04-22 11:48:51 +0530

    cors fixing

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(cors({
 app.use(express.json())
 
 app.get("/", (req, res) => {
-    res.send("server running")
+    res.send(`server running${process.env.ORIGIN_URL}`)
 })
 
 app.use("/users", Validator, userRouter)
